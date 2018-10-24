4ffef81e4cd88e1c8e30251e994b3ad8c84426f0
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-10-24 09:53:50 +0200

    Kubeadm setup update

--- kubeadm/02-bootstrap-first-server.sh
+++ kubeadm/02-bootstrap-first-server.sh
@@ -14,4 +14,4 @@ mkdir -p bootstrap
 gcloud compute ssh --ssh-key-file kube-rsa $host --zone $ZONE --command "sudo tar cjvf kubernetes.tar.bz2 /etc/kubernetes/pki/ca.crt /etc/kubernetes/pki/ca.key /etc/kubernetes/pki/sa.key /etc/kubernetes/pki/sa.pub /etc/kubernetes/pki/front-proxy-ca.crt /etc/kubernetes/pki/front-proxy-ca.key /etc/kubernetes/pki/etcd/ca.crt /etc/kubernetes/pki/etcd/ca.key /etc/kubernetes/admin.conf"
 gcloud compute scp --ssh-key-file kube-rsa $host:~/kubernetes.tar.bz2 bootstrap/kubernetes.tar.bz2 --zone $ZONE
 
-gcloud compute ssh --ssh-key-file kube-rsa $host --zone $ZONE --command "sudo mkdir /root/.kube && sudo cp /etc/kubernetes/admin.conf /root/.kube/config"
+gcloud compute ssh --ssh-key-file kube-rsa $host --zone $ZONE --command "sudo mkdir -p /root/.kube && sudo cp /etc/kubernetes/admin.conf /root/.kube/config"
